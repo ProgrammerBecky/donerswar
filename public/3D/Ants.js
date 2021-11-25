978a1b7a8af8cc07be3bec7e0f968f5cc92ed635
= <=>
2021-11-25 22:41:09 +0000

    Mech Death

--- public/3D/Ants.js
+++ public/3D/Ants.js
@@ -267,7 +267,10 @@ export class Ants {
 
 			console.log( 'making collective decision' );
 
-			const targetMech = Math.floor( G.mechs.mechs.length * Math.random() );
+			do {
+				const targetMech = Math.floor( G.mechs.mechs.length * Math.random() );
+			}
+			while( ! G.mechs.mechs[ targetMech ].active );
 			self.postMessage({
 				type: 'ant-navigate',
 				sx: 0,
