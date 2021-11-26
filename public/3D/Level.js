6dee0c0c3c310fba536bc0802805edb828786cf8
= <=>
2021-11-26 22:20:09 +0000

    Level changes

--- public/3D/Level.js
+++ public/3D/Level.js
@@ -5,7 +5,7 @@ export class Level {
 
 	constructor() {
 		this.level = 1;
-		this.stage = 1;
+		this.stage = 0;
 	
 		this.levelCheckTimer = 0;
 	
@@ -27,8 +27,6 @@ export class Level {
 			roughness: 1,
 			envMap: G.environmentMap,
 			envMapIntensity: 0.5,
-			transparent: true,
-			opacity: 0.9,
 		}) );	
 		const geo = new THREE.CylinderGeometry( 200 , 200 , 6000 , 16 , 1 , true );
 		this.disco = new THREE.Mesh( geo , discoMat );
@@ -67,83 +65,183 @@ export class Level {
 		G.ants.spawnAnt( 10000 , 51684 );
 		G.ants.spawnAnt( 45000 , 51684 );
 		G.ants.spawnAnt( 10000 , 21684 );
+		
+		this.stages = [
+			{
+				disco: {x: 21575, z: 57973},
+				ants: {maximumAnts: 5, headJumpChance: 0},
+				spawnAhead: 1,
+				spawnBehind: 0,
+				rewardMech: false,
+			},
+			{
+				disco: { x: 19048, z: 43083},
+				ants: {maximumAnts: 10, headJumpChance: 0.05},
+				spawnAhead: 3,
+				spawnBehind: 0,
+				rewardMech: 1,
+			},
+			{
+				disco: { x: 33371, z: 46884},
+				ants: { maximumAnts: 10, headJumpChance: 0.1},
+				spawnAhead: 4,
+				spawnBehind: 4,
+				rewardMech: false,
+			},
+			{
+				disco: { x: 11179, z: 63870},
+				ants: { maximumAnts: 12, headJumpChance: 0.1},
+				spawnAhead: 5,
+				spawnBehind: 10,
+				rewardMech: false,
+			},
+			{
+				disco: { x: 12325 , z: 37064 },
+				ants: { maximumAnts: 15, headJumpChance: 0.15},
+				spawnAhead: 10,
+				spawnBehind: 5,
+				rewardMech: 2,
+			},
+			{
+				disco: { x: 10664, z: 14927 },
+				ants: { maximumAnts: 15, headJumpChance: 0.2},
+				spawnAhead: 10,
+				spawnBehind: 10,
+				rewardMech: false,
+			},
+			{
+				disco: { x: 19819, z: 7500 },
+				ants: { maximumAnts: 15, headJumpChance: 0.3},
+				spawnAhead: 10,
+				spawnBehind: 10,
+				rewardMech: false,
+			},
+			{
+				disco: { x: 35081 , z: 11674},
+				ants: { maximumAnts: 20, headJumpChance: 0.4},
+				spawnAhead: 20,
+				spawnBehind: 20,
+				rewardMech: 3,
+			},
+			{
+				disco: { x: 38770 , z: 26410},
+				ants: { maximumAnts: 25, headJumpChance: 0.5},
+				spawnAhead: 20,
+				spawnBehind: 20,
+				rewardMech: false,
+			},
+			{
+				disco: { x: 54385 , z: 10279},
+				ants: { maximumAnts: 10, headJumpChance: 0.5},
+				spawnAhead: 5,
+				spawnBehind: 5,
+				rewardMech: false,
+			},
+			{
+				disco: { x: 68956 , z: 21360},
+				ants: { maximumAnts: 12, headJumpChance: 0.5},
+				spawnAhead: 10,
+				spawnBehind: 10,
+				rewardMech: false,
+			},
+			{
+				disco: { x: 77455 , z: 47863},
+				ants: { maximumAnts: 15, headJumpChance: 0.5},
+				spawnAhead: 12,
+				spawnBehind: 12,
+				rewardMech: false,
+			},
+			{
+				disco: { x: 72385 , z: 67290},
+				ants: { maximumAnts: 15, headJumpChance: 0.5},
+				spawnAhead: 12,
+				spawnBehind: 12,
+				rewardMech: false,
+			},
+			{
+				disco: { x: 64766 , z: 77392},
+				ants: { maximumAnts: 25, headJumpChance: 0.5},
+				spawnAhead: 20,
+				spawnBehind: 20,
+				rewardMech: false,
+			},
+			{
+				disco: { x: 42500 , z: 42500},
+				ants: { maximumAnts: 50, headJumpChance: 1},
+				spawnAhead: 50,
+				spawnBehind: 50,
+				rewardMech: false,
+			},
+		];
+		
 	}
 	checkLevelGroover() {
 		
-		if( this.stage === 1 ) {
-			if( this.checkMechPos({ rng: 1000 }) ) {
-				this.stage++;
+		let victory = false;
+		for( let i=0 ; i<G.mechs.mechs.length ; i++ ) {
+			if( G.mechs.mechs[i].active ) {
+				const dx = this.disco.position.x - G.mechs.mechs[i].x;
+				const dz = this.disco.position.z - G.mechs.mechs[i].z;
+				const dr = Math.sqrt( dx*dx + dz*dz )
+				if( dr < 1000 ) {
+					victory=true;
+					console.log( 'MECH ' + i + ' TRIGGERED STAGE ' + this.stage + ' VICTORY' );
+				}
+			}
+		}
+		
+		if( victory ) {
+			const data = this.stages[ this.stage ];
+
+			G.ants.maximumAnts = data.ants.maximumAnts;
+			G.ants.headJumpChance = data.ants.headJumpChance;
+			
+			for( let i=0 ; i<data.spawnAhead ; i++ ) {
+				G.ants.spawnAnt(
+					data.disco.x + Math.random() * 5000 - 2500,
+					data.disco.z + Math.random() * 5000 - 2500
+				);
+			}
+			
+			if( data.spawnBehind > 0 ) {
+				const oldDisco = this.stages[ this.stage-2 ].disco;
+				for( let i=0 ; i<data.spawnBehind ; i++ ) {
+					G.ants.spawnAnt(
+						oldDisco.x + Math.random() * 5000 - 2500,
+						oldDisco.z + Math.random() * 5000 - 2500
+					);
+				}
+			}
+
+			this.disco.position.set( data.disco.x , 1500 , data.disco.z );
+
+			if( data.rewardMech ) {
+				
+				const spawnDisco = this.stages[ this.stage-1 ].disco;
+				
 				self.postMessage({
 					type: 'music',
-					music: 'stage-victory',
+					music: 'mechunlock' + data.rewardMech,
 					volume: 0.3,
 				});
-				this.disco.position.set(21575,1500,57973);
-				G.ants.maximumAnts = 5;
-				G.ants.headJumpChance = 0;
-				G.ants.spawnAnt( 21575, 57973 );				
-				G.mechs.mechs[1].x = 10000;
-				G.mechs.mechs[1].z = 51684;
-				G.mechs.mechs[1].active = true;
+				
+				G.mechs.mechs[ data.rewardMech ].x = spawnDisco.x;
+				G.mechs.mechs[ data.rewardMech ].z = spawnDisco.z;
+				G.mechs.mechs[ data.rewardMech ].active = true;
+				G.mechs.updatePositions();
+				
 				self.postMessage({
 					type: 'mech-status',
-					mechId: 1,
+					mechId: data.rewardMech,
 					status: true,
 				});						
 			}
+
+			this.stage++;
+
 		}
-		else if( this.stage === 2 ) {
-			if( this.checkMechPos({ rng: 1000 }) ) {
-				this.stage++;
-				this.disco.position.set( 19048.259, 1500 , 43083 );
-			}
-		}
-		else if( this.stage === 3 ) {
-			if( this.checkMechPos({ rng: 8000 }) ) {
-				this.stage++;
-				self.postMessage({
-					type: 'music',
-					music: 'victory',
-					volume: 0.3,
-				});
-				G.ants.maximumAnts = 10;
-				G.ants.headJumpChance = 0.05;
-				//G.ants.spawnAnt( 21575, 57973 );				
-				G.mechs.mechs[2].x = 19050;
-				G.mechs.mechs[2].z = 43083;
-				G.mechs.updatePositions();
-				G.mechs.mechs[2].active = true;
-				self.postMessage({
-					type: 'mech-status',
-					mechId: 1,
-					status: true,
-				});	
-/*
-				self.postMessage({
-					type: 'music',
-					music: 'stage-three',
-					volume: 0.3,
-				});
-			*/
-			}
-		}
-		
+
 	}
 		
-	checkMechPos({ x=false,z=false,rng }) {
-		
-		if( ! x ) x = this.disco.position.x;
-		if( ! z ) z = this.disco.position.z;
-		
-		for( let i=0 ; i<G.mechs.mechs.length ; i++ ) {
-			if( G.mechs.mechs[i].active ) {
-				const dx = x - G.mechs.mechs[i].x;
-				const dz = z - G.mechs.mechs[i].z;
-				const dr = Math.sqrt( dx*dx + dz*dz )
-				if( dr < rng ) return true;
-			}
-		}
-		return false;
-	}
 
 }
